d02595ae400e4780737f71090de2ce50e5acdb55
rakshitDarji007 <rakshit.darji007@gmail.com>
2025-08-13 22:46:56 -0500

    updated the coin to finish the game

--- pit-escape/player.gd
+++ pit-escape/player.gd
@@ -2,15 +2,24 @@ extends CharacterBody2D
 
 @export var speed = 300.0
 @export var jump_velocity = -400.0
+@export var max_jumps = 2
 
+var jumps_made = 0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _ready():
+	add_to_group("player")
 
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
+	else:
+		jumps_made = 0
 
-	if Input.is_action_just_pressed("Up") and is_on_floor():
-		velocity.y = jump_velocity
+	if Input.is_action_just_pressed("jump"):
+		if jumps_made < max_jumps:
+			velocity.y = jump_velocity
+			jumps_made += 1
 
 	var direction = Input.get_axis("Left", "Right")
 	if direction:
@@ -19,3 +28,6 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
+
+func _on_coin_collected():
+	get_tree().change_scene_to_file("res://win_screen.tscn")
